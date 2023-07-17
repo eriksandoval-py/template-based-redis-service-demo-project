46fad57525204ff0eb23f51b64c70097edc68792
Erik Sandoval <eriksandoval.py@gmail.com>
2023-07-16 21:58:11 -0500

    Initial creation.

---
+++ src/main/java/com/eriksandoval/templatebasedredisservicedemoproject/TemplateBasedRedisServiceDemoProjectApplication.java
@@ -0,0 +1,13 @@
+package com.eriksandoval.templatebasedredisservicedemoproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemplateBasedRedisServiceDemoProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemplateBasedRedisServiceDemoProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eriksandoval/templatebasedredisservicedemoproject/TemplateBasedRedisServiceDemoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eriksandoval.templatebasedredisservicedemoproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemplateBasedRedisServiceDemoProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
